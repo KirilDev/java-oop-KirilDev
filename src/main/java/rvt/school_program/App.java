10922ebea92f7616aad91f6fc73589823dd6fb7c
KirilDev <alexprogrammer2023@gmail.com>
2026-01-07 15:45:24 +0200

    personFirst object created

--- src/main/java/rvt/school_program/App.java
+++ src/main/java/rvt/school_program/App.java
@@ -2,6 +2,9 @@ package rvt.school_program;
 
 public class App {
     public static void main(String[] args){
-        
+        Person personFirst = new Person("Kirill", "N/A", "Krišjana Valdemara", "students@rvt.lv");
+        personFirst.getName();
+
+        // personFirst.name = "Alex"; | Doesn't work because person field isn't visible.
     }
 }
