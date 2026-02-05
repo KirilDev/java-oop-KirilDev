59e2464e9951df8328e28106d4c2d6cb0b56c26b
KirilDev <alexprogrammer2023@gmail.com>
2026-02-05 12:48:24 +0200

    Clock in

---
+++ src/main/java/rvt/todo/ToDoList.java
@@ -0,0 +1,13 @@
+package rvt.todo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ToDoList {
+    public static void main(String[] args){
+        File file = new File("/todo.csv");
+
+        Scanner reader = new Scanner(System.in);
+
+    }
+}
