7490a3236dab2d9dbdea56bbfb0a824bdd27ed4a
Kirill <126055944+KirilDev@users.noreply.github.com>
2026-02-18 09:10:40 +0000

    Impletemted Student Adding Feature, Not Debugged :(

--- src/main/java/rvt/student_registration_system/App.java
+++ src/main/java/rvt/student_registration_system/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     
     private static final String DATA_ACCESS = "data/students.cvs";
@@ -8,7 +10,89 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         while(true){
+            System.out.println("Welcome to college's student registration system! ");
+            System.out.println(" ");
+            System.out.println("Select action to complete: ");
+            System.out.println("1 -> Add New Student");
+            System.out.println("2 -> View Students Table");
+            System.out.println("3 -> Remove Student by ID");
+            System.out.println("4 -> Update Student Info by ID");
+            System.out.println("5 -> Exit");
+
             
+            int userInput = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userInput) {
+                case 1:
+                    addNewStudent(scanner);
+                    break;
+                case 2:
+                    loadStudentTable();    
+                    break;
+
+                case 3:
+                    removeStudentById();
+                    break;
+
+                case 4:
+                    updateStudentById();     
+                    break;
+
+                    case 5:
+                        System.exit(0);
+                    break;
+
+            
+                default:
+                    System.out.println("Error during program work!...");
+                    main(null);
+                    break;
+            }
         }
+    }
+
+    private static void addNewStudent(Scanner scanner){
+        System.out.println("Enter Students Name: ");
+        String studentName = scanner.nextLine();
+
+        System.out.println("Enter Students Surname: ");
+        String studentSurname = scanner.nextLine();
+
+        System.out.println("Enter Students Email: ");
+        String studentEmail = scanner.nextLine();
+
+        System.out.println("Enter Students Class: ");
+        String studentClass = scanner.nextLine();
+
+        System.out.println("Enter Students Program: ");
+        String studentProgram = scanner.nextLine();
+
+        System.out.println("Enter Students Personal Code: ");
+        String studentPersonalCode = scanner.nextLine();
+
+
+        int Id = students.isEmpty() ? 1 : students.get(students.size() - 1).getId() + 1;
+        students.add(new Student(Id, studentName, studentSurname, studentEmail, studentClass, studentProgram, studentPersonalCode));
+
+        fileSaver();
+
+        System.out.println("New student added!");
+    }
+
+    private static void fileSaver() {
+
+    }
+
+    private static void loadStudentTable() {
+
+    }
+
+    private static void removeStudentById() {
+
+    }
+
+    private static void updateStudentById() {
+
     }
 }
